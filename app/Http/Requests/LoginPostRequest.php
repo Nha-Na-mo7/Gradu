e906df8ea14950774f70984774b0a68e37b583d1
Nhala <zirconiump42@gmail.com>
2020-09-29 17:28:18 +0900

    ログイン用のフォームリクエスト(ただし使うかは不明)

--- app/Http/Requests/LoginPostRequest.php
+++ app/Http/Requests/LoginPostRequest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class LoginPostRequest extends FormRequest
 {
+    private $PASSWORD_MIN_LENGTH = 8;
+  
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -13,7 +15,7 @@ class LoginPostRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,8 +25,26 @@ class LoginPostRequest extends FormRequest
      */
     public function rules()
     {
+        extract(get_object_vars($this));
+    
         return [
-            //
+            'email' => "required|email|max:100}",
+            'password' => "required|min:{$PASSWORD_MIN_LENGTH}"
         ];
+    }
+    
+    public function messages()
+    {
+        // グローバル変数を記述しやすくするもの
+        extract(get_object_vars($this));
+    
+        return [
+            'email.required' => '入力してください',
+            'email.email' => 'メールアドレスの形式で入力してください',
+            'email.max' => "100文字以内で入力してください",
+            'password.required' => '入力してください',
+            'password.min' => "{$PASSWORD_MIN_LENGTH}文字以上で入力してください",
+        ];
+        
     }
 }
