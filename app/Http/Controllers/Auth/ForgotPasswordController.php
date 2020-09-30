b12cc9c960863dea813da6bdde7fcfd400afdbc2
Nhala <zirconiump42@gmail.com>
2020-09-30 10:15:18 +0900

    #ForgotPasswordController DBに存在しないメールアドレスが入力された場合でも「送信した」メッセージを表示するように変更 (DBに存在しない場合、実際にはメールは送信されない)

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
 
 class ForgotPasswordController extends Controller
 {
@@ -19,4 +20,25 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+  
+    /**
+     * SendsPasswordResetEmails trait で定義されているメソッドをオーバーライド。
+     * DBにアドレスが登録されているか否かを問わず、「送信しました」とメッセージを出す。
+     * ただしDBに登録されていないメアドの場合、実際にはメールを送らない。
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $response
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
+     */
+    protected function sendResetLinkFailedResponse(Request $request, $response)
+    {
+      // もともとはこちらの処理
+      //return back()
+      //        ->withInput($request->only('email'))
+      //        ->withErrors(['email' => trans($response)]);
+      // レスポンスを成功したときと同様の処理に書き換える。
+      $response = 'passwords.sent';
+      // 成功時と同じようにバリデーションエラーは出さないで戻す
+      return back()->with('status', trans($response));
+    }
 }
