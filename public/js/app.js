89049d1e6a99873e1f9d6415e36c335ef4f2a830
Nhala <zirconiump42@gmail.com>
2020-10-07 20:15:44 +0900

    #SASS News.vueのクラス変更 ・c-news__item → p-news__item

--- public/js/app.js
+++ public/js/app.js
@@ -22827,7 +22827,7 @@ var staticRenderFns = [
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "c-news__item" }, [
+    return _c("div", { staticClass: "p-news__item" }, [
       _c("h2", [_vm._v("ニュース")])
     ])
   }
