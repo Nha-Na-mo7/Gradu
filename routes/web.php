12faa2636a43f2f851607c12dc0f7220b26ae819
Nhala <zirconiump42@gmail.com>
2020-10-20 09:07:28 +0900

    #socialite routes/web.php へルーティングを記述 ・login/twitter の方にアクセスするとTwitter経由での認証処理が始まります。 ・login/twitter/callback はTwitterのアプリ側から情報が帰ってくるURLです。
    流れを単純化すると、
    
    ①login/twitterのURLをユーザーが踏む
    ↓
    ②Twitterアプリ側が認証をする
    認証が成功したら
    ↓
    ③login/twitter/callbackへユーザー情報を返すので
    それをUserテーブルに保管する
    
    という流れになっています。
    
    ルーティングはできたので肝心なControllerへの処理を書いていきます。

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// ================
+// Twitter認証用
+// ================
+// ①login/twitterのURLをユーザーが踏む
+// ↓
+// ②Twitterアプリ側が認証をする
+// 認証が成功したら...
+// ↓
+// ③login/twitter/callbackへユーザー情報を返すので
+// それをUserテーブルに保管する
+
+// Twitter経由での認証を開始するURL
+Route::get('login/twitter', 'Auth\LoginController@redirectToTwitterProvider');
+// Twitterアプリケーション側から情報が返ってくるURL
+Route::get('login/twitter/callback', 'Auth\LoginController@handleTwitterProviderCallback');
+
+
+// ================
+// other
+// ================
 // あらゆるURLはindex.blade.phpを返す
 Route::get('/{any?}', fn() => view('index'))->where('any', '.+');
+
