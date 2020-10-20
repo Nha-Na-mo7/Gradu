4adc0af23f2f31222b761f1ee17949641fcb294e
Nhala <zirconiump42@gmail.com>
2020-10-20 16:44:46 +0900

    メモ...Twitterによる新規登録に成功 #.envファイルで、ポート番号つきのURLに設定する >CALLBACK_URL=http://localhost:3000/login/twitter/callback
    #TwitterAPP管理ツールにて、CALLBACK URLSもポート番号付きのURLに設定する
    >http://localhost:3000/login/twitter/callback
    
    ・DBに登録がされていることを確認。
    ・もちろんパスワードカラムには何も入っていないので、nullableにするのか、仮パスワードを設定してあげるのかはTODO

--- public/js/app.js
+++ public/js/app.js
@@ -62974,15 +62974,14 @@ __webpack_require__.r(__webpack_exports__);
 /*!*********************************************************!*\
   !*** ./resources/js/pages/AccountLists/AccountList.vue ***!
   \*********************************************************/
-/*! no static exports found */
+/*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _AccountList_vue_vue_type_template_id_231ee6a6_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./AccountList.vue?vue&type=template&id=231ee6a6&scoped=true& */ "./resources/js/pages/AccountLists/AccountList.vue?vue&type=template&id=231ee6a6&scoped=true&");
 /* harmony import */ var _AccountList_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./AccountList.vue?vue&type=script&lang=js& */ "./resources/js/pages/AccountLists/AccountList.vue?vue&type=script&lang=js&");
-/* harmony reexport (unknown) */ for(var __WEBPACK_IMPORT_KEY__ in _AccountList_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__) if(["default"].indexOf(__WEBPACK_IMPORT_KEY__) < 0) (function(key) { __webpack_require__.d(__webpack_exports__, key, function() { return _AccountList_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__[key]; }) }(__WEBPACK_IMPORT_KEY__));
-/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+/* empty/unused harmony star reexport *//* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
 
 
 
@@ -63012,7 +63011,7 @@ component.options.__file = "resources/js/pages/AccountLists/AccountList.vue"
 /*!**********************************************************************************!*\
   !*** ./resources/js/pages/AccountLists/AccountList.vue?vue&type=script&lang=js& ***!
   \**********************************************************************************/
-/*! no static exports found */
+/*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
