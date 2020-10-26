0f1d14e1ea152f5eb46ed4f8232eb350ca6c1bf0
Nhala <zirconiump42@gmail.com>
2020-10-26 09:51:17 +0900

    TwitterController GoogleNewsControllerのget_news()を元にして、 メソッド「get_accounts()」を新規作成。 ・まだ処理も何もない。 ・GETメソッドで、アカウント一覧を取得する予定

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class TwitterController extends Controller
 {
-    //
+    // 該当のTwitterアカウントを取得する
+    public function get_accounts() {
+  
+      // TODO getキーワードにスクリプト攻撃が混ざる可能性を考察すること
+      // GETパラメータの値を元に、Twitterアカウントを取得する
+      // 「仮想通貨」がプロフィール、またはアカウント名に含まれていること
+      $keywords = !empty($_GET['keywords']) ? $_GET['keywords'] : '仮想通貨';
+      // $max_num = 100;
+  
+      // 実行時間。90秒。
+      set_time_limit(90);
+  
+  
+      
+      
+    }
 }
