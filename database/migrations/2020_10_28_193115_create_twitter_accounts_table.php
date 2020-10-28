278e4131d80b6fe838da3f3fe7c255644a16f7c2
Nhala <zirconiump42@gmail.com>
2020-10-28 19:57:02 +0900

    twitter_accountsテーブルを新規作成 ・タイムスケジューラによるバッチ処理で収集したTwitterアカウントを保存するためのテーブル、及びそのマイグレーションファイル。 php artisan make:model TwitterAccount --migration
    作成項目は以下の通り
    列名	型	PRIMARY	UNIQUE	 NOT NULL	備考
    id	BIGINTEGER	🔑	✅	✅	テーブルのid
    account_id	INTEGER		✅	✅	アカウントのid
    name	STRING			✅	アカウント名
    screen_name	STRING			✅	@から始まるユーザーネーム
    description	TEXT				プロフィール
    protected	boolean			✅	鍵垢かどうか(使わないかもしれない)
    friends_count	INTEGER			✅	フォロー数
    followers_count	Integer			✅	フォロワー数
    status_id_str	STRING		✅		最新ツイートのID
    status_text	TEXT				最新ツイートの内容
    status_created_at	TIMESTAMP				最新ツイートの投稿時刻
    profile_image_url_https	STRING				アカウントのアイコンURL
    ※ following	boolean			✅	連携しているアカウントが、この人物をフォロー済かどうか
    
    ※...一旦不要なのでコメントアウト

---
+++ database/migrations/2020_10_28_193115_create_twitter_accounts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTwitterAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('twitter_accounts', function (Blueprint $table) {
+            $table->bigIncrements('id'); // プライマリーキー
+            $table->integer('account_id')->unique(); // 取得したTwitterアカウントのID
+            $table->string('name'); // アカウント名
+            $table->string('screen_name')->unique(); // @から始まるユーザーネーム
+            $table->text('description')->nullable(); // プロフィール
+            $table->boolean('protected'); // 鍵アカウントであるか
+            $table->integer('friends_count'); // フォロー数
+            $table->integer('followers_count'); // フォロワー数
+            $table->string('status_id_str')->unique()->nullable(); // 最新ツイートのID
+            $table->text('status_text')->nullable(); // 最新ツイートの内容
+            $table->timestamp('status_created_at')->nullable(); // 最新ツイートの投稿時刻
+            $table->string('profile_image_url_https')->nullable(); // 画像アイコンのURL
+            // $table->boolean('following'); // 連携済みユーザーがこのユーザーをフォローしているか
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('twitter_accounts');
+    }
+}
