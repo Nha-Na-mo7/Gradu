783edb4235375aee27da3263b651fb5c0b355391
Nhala <zirconiump42@gmail.com>
2020-10-28 13:32:54 +0900

    Paginationコンポーネント を作成

---
+++ resources/js/pages/Components/Pagination.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="c-pagination">
+    <RouterLink
+        v-if="! isFirstPage"
+        :to="`/?p=${currentPage - 1}`"
+        class="button"
+    >&laquo; prev</RouterLink>
+    <RouterLink
+        v-if="! isLastPage"
+        :to="`/?p=${currentPage + 1}`"
+        class="button"
+    >next &raquo;</RouterLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentPage: {
+      type: Number,
+      required: true
+    },
+    lastPage: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    isFirstPage() {
+      return this.currentPage === 1
+    },
+    isLastPage() {
+      return this.currentPage === this.lastPage
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
