344f2a3e40366a17d90cf9afb7deb70a5e134627
Nhala <zirconiump42@gmail.com>
2020-10-30 17:45:39 +0900

    BrandTweetCount / マイグレーション DBに詳しく作成 取得インターバルもこのテーブルで管理する。(1時間ごと、24時間ごと、7日ごとといった間隔ごとにテーブルで分けない)

--- database/migrations/2020_10_30_170944_create_brand_tweet_counts_table.php
+++ database/migrations/2020_10_30_170944_create_brand_tweet_counts_table.php
@@ -15,7 +15,18 @@ class CreateBrandTweetCountsTable extends Migration
     {
         Schema::create('brand_tweet_counts', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('brand_id')->unsigned(); // 通貨ID
+            $table->integer('tweet_count')->unsigned(); // 取得時点でのツイート数
+            $table->integer('tweet_count_before')->unsigned(); // 直前のツイート数
+            $table->integer('interval_hour')->unsigned(); // ツイート取得間隔、1時間単位
             $table->timestamps();
+    
+            //外部キーでtwitter_accountsのaccount_idと紐付け。
+            //主テーブルのレコードが削除されたら、このテーブルのデータも一緒に消える。
+            $table->foreign('brand_id')
+                ->references('id')
+                ->on('brands')
+                ->onDelete('cascade');
         });
     }
 
