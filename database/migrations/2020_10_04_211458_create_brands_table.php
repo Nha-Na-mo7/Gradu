01cc7f985240de2a04c481bb9908df0d7ca86f59
Nhala <zirconiump42@gmail.com>
2020-11-12 11:09:57 +0900

    Brand カタカナでの読み方を追加

--- database/migrations/2020_10_04_211458_create_brands_table.php
+++ database/migrations/2020_10_04_211458_create_brands_table.php
@@ -15,7 +15,8 @@ class CreateBrandsTable extends Migration
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name')->unique();
+            $table->string('name')->unique()->comment('アルファベットの略称名');
+            $table->string('realname')->unique()->comment('カタカナでの一般的な読み方');
             $table->string('icon')->nullable()->comment('svgアイコンのパス');
             $table->boolean('handling')->comment('CoinCheckで現在取扱中の銘柄であるか');
         });
