1940d866a626820738ab56caaa387835709a5b3a
Nhala <zirconiump42@gmail.com>
2020-11-18 16:31:09 +0900

    BTCしか取得できないが、CoinCheckAPIからtickerを取得し、価格をDBに格納するまでの一連処理メソッドを記述 >refresh後にテストし、kernelに記述する。

--- app/Http/Controllers/CoinCheckController.php
+++ app/Http/Controllers/CoinCheckController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CoincheckPrice;
 use Coincheck\Coincheck;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -13,18 +14,53 @@ class CoinCheckController extends Controller
      * TODO 大多数の通貨で24時間以内の取引価格が"不明"になってしまうがそれでも良いか確認する
      */
   
+    // brandsテーブルの通貨ID
+    const BTC_ID = 1;
+  
+    // ============================================================
+    // バッチ用・24時間に1度、24時間最高or最安取引価格を取得しDB保存する
+    // ============================================================
+    public function daily_price_check(){
+      Log::debug('=======================================');
+      Log::debug('定期 CoinCheckController.daily_price_check');
+      Log::debug('=======================================');
+      
+      
+      // BTCのtickerを取得する
+      Log::debug('---- BTCの取引価格を取得し保存します。 ----');
+      $btc = $this->get_btc_ticker();
+      
+      // 最低取引価格
+      $price_min = $btc->low;
+      // 最高取引価格
+      $price_max = $btc->high;
+      
+      Log::debug('最低取引価格: '.$price_min.' 最高取引価格:'.$price_max);
+      $this->insert_price_to_db(self::BTC_ID, $price_min, $price_max);
+      Log::debug('---- BTCの処理が終了しました。 ----');
+  
+  
+      Log::debug('====================================');
+      Log::debug('▲▲▲▲▲▲▲  処理が全て完了しました。 ▲▲▲▲▲▲▲');
+      Log::debug('====================================');
+    }
+  
+  
+  
+  
+    
     // ==========================
     // BTCのティッカーを取得する
     // ==========================
-    public function get_ticker(){
+    private function get_btc_ticker(){
       // high 24時間での最高取引価格
       // low 24時間での最安取引価格
-      // 他にも取得可能な情報はある
       
       // コインチェックインスタンスを作成
       $coincheck = $this->make_coincheck_instanse();
       
       // tickerを取得する
+      Log::debug('CoinCheckのAPIを使ってBTCのティッカーを取得します。');
       $response = $coincheck->ticker->all();
       Log::debug('$response :'.print_r($response, true));
       
@@ -34,12 +70,17 @@ class CoinCheckController extends Controller
     // ==========================
     // 24時間での取引価格をDBに保存する
     // ==========================
-    
-  
-  
-  
-  
-    
+    private function insert_price_to_db($brand_id, $price_min, $price_max){
+      Log::debug('DBに24時間の最高or最低取引価格を保存します。');
+      // モデルを作成しinsertする
+      $coinchech_price = new CoincheckPrice();
+      
+      $coinchech_price->fill([
+          'brand_id' => $brand_id,
+          'price_min' => $price_min,
+          'price_max' => $price_max
+      ])->save();
+    }
     
     // ==========================
     // Coincheckインスタンスの作成
--- app/Models/CoincheckPrice.php
+++ app/Models/CoincheckPrice.php
@@ -6,7 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class CoincheckPrice extends Model
 {
-    //
+    // JSONに含める属性
+    // account_idはtwitter_accountsテーブルのものを参照するのでこちらでは不要
+    protected $visible = [
+        'brand_id',
+        'price_min',
+        'price_max',
+        'updated_at'
+    ];
+    
+    // fillable
+    protected $fillable = [
+        'account_id',
+        'tweet_id_str',
+        'tweet_text',
+        'tweet_created_at',
+    ];
   
   
     /**
