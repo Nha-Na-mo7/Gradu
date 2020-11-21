e4ca5726648f17c4b03f9275e6eebd4ec54d29e9
Nhala <zirconiump42@gmail.com>
2020-11-21 10:23:47 +0900

    退会をusersに メールのバリデーション

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -19,45 +19,5 @@ class MypageController extends Controller
      * ・退会処理など
      */
   
-    
-    // ======================
-    // 退会する
-    // ======================
-    public function withdraw()
-    {
-      try {
-        // 認証済みユーザーを取得
-        $user = Auth::user();
-  
-        // ログアウト、update処理が行われる
-        Auth::logout();
-        
-        // ユーザーを削除する
-        $user->delete();
-        
-        // セッションを一度消してから再発行
-        session()->invalidate();
-        // csrfトークンを再生成
-        session()->regenerateToken();
-        
-        // リダイレクト処理はフロントエンドで行う
-        
-        return response()->json(['success'], 200);
-        
-      } catch (\Exception $e) {
-        
-        // ログアウト
-        Auth::logout();
-        // セッションを一度消してから再発行
-        session()->invalidate();
-        // csrfトークンを再生成
-        session()->regenerateToken();
-        
-        Log::debug('退会処理の過程でエラーです。'. $e->getMessage());
-        
-        return response()->json(['error', 'エラーが発生しました。'], 500);
-      }
-    }
-  
   
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,5 +15,45 @@ class UserController extends Controller
       // Log::debug('UserController.auth_user 認証中のユーザー情報を返却します。');
       return Auth::user();
     }
-
+  
+  
+    // ======================
+    // 退会する
+    // ======================
+    public function withdraw()
+    {
+      try {
+        // 認証済みユーザーを取得
+        $user = Auth::user();
+        
+        // ログアウト、update処理が行われる
+        Auth::logout();
+        
+        // ユーザーを削除する
+        $user->delete();
+        
+        // セッションを一度消してから再発行
+        session()->invalidate();
+        // csrfトークンを再生成
+        session()->regenerateToken();
+        
+        // リダイレクト処理はフロントエンドで行う
+        
+        // TODO XXXX
+        return response()->json(['success'], 200);
+        
+      } catch (\Exception $e) {
+        
+        // ログアウト
+        Auth::logout();
+        // セッションを一度消してから再発行
+        session()->invalidate();
+        // csrfトークンを再生成
+        session()->regenerateToken();
+        
+        // TODO XXXX
+        Log::debug('退会処理の過程でエラーです。'. $e->getMessage());
+        return response()->json(['error', 'エラーが発生しました。'], 500);
+      }
+    }
 }
