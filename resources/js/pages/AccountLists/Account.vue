39d32e0b6cc075d5a358f6c14aa359393e8c832b
Nhala <zirconiump42@gmail.com>
2020-11-29 18:21:39 +0900

    アカウント一覧ようコンポーネント 仮のデザイン完成 この後タグをふさわしいものに修正する

--- resources/js/pages/AccountLists/Account.vue
+++ resources/js/pages/AccountLists/Account.vue
@@ -2,7 +2,7 @@
 <!--アカウント一覧画面でいくつも描画される各twitterアカウントの情報-->
 <!--=======================================================-->
 
-<!-- item2~7のクラス、及びscope内のスタイルは、本番に上げる前に外してください！ -->
+<!-- TODO item2~7のクラス、及びscope内のスタイルは、本番に上げる前に外してください！ -->
 
 
 <template>
@@ -32,96 +32,95 @@
       <!-- プロフィールとフォロー関連のエリア -->
       <div class="item-3 p-accounts__data--area">
 
-        <!-- プロフィールエリア -->
+        <!-- アカウント名など -->
         <div class="item-4 p-accounts__profile--area">
-          <div class="item-5 p-accounts__profile">
-            <!-- アカウント名 -->
-            <div class="item-6 p-accounts__profile--nickname">
-              <p>
-                <a
-                    :href="twitter_account_url"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                  {{ account.name }}
-                </a>
-              </p>
-            </div>
-            <!-- Twitterユーザー名 -->
-            <div class="item-6 p-accounts__profile--username">
-              <span>
-                <a
-                    :href="twitter_account_url"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                  {{ account_screen_name |  add_AtSign_to_screen_name }}
-                </a>
-              </span>
-            </div>
+          <!-- アカウント名 -->
+          <div class="item-6 p-accounts__profile--nickname">
+            <p>
+              <a
+                  :href="twitter_account_url"
+                  target="_blank"
+                  rel="noopener noreferrer"
+              >
+                {{ account.name }}
+              </a>
+            </p>
+          </div>
+          <!-- Twitterユーザー名 -->
+          <div class="item-6 p-accounts__profile--username">
+          <span>
+            <a
+                :href="twitter_account_url"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              {{ account_screen_name |  add_AtSign_to_screen_name }}
+            </a>
+          </span>
           </div>
 
-          <!-- Twitterプロフィール -->
-          <div
-              class="item-5 p-accounts__profile--description"
-              v-if="isExist_profile_description"
-          >
-            <p>{{ account.description }}</p>
-          </div>
         </div>
 
-        <!-- フォローボタンとFF数のエリア -->
-        <div class="item-4 p-accounts__follow--area">
 
-          <!-- フォローボタンエリア、そのユーザーがTwitterアカウントを連携していない場合非表示 -->
-          <div class="item-5 p-accounts__follow-btn--area">
-            <!-- フォローしていないアカウントを優先表示するので、フォローしているアカウントはページ更新すると出てこなくなる-->
-            <button
-                v-if="isFollowing"
-                class="c-btn c-btn__follow c-btn__follow--destroy"
-                :class="{'c-btn__disabled': isAutoFollowing}"
-                :disabled="isAutoFollowing"
-                @click="destroy"
-            >フォロー解除</button>
-            <button
-                v-else
-                class="c-btn c-btn__follow"
-                :class="{'c-btn__disabled': isAutoFollowing}"
-                :disabled="isAutoFollowing"
-                @click="follow"
-            >フォローする</button>
-          </div>
+        <!-- フォローボタンエリア -->
+        <div class="item-5 p-accounts__follow-btn--area">
+          <button
+              v-if="isFollowing"
+              class="c-btn c-btn__follow c-btn__follow--destroy"
+              :class="{'c-btn__disabled': isAutoFollowing}"
+              :disabled="isAutoFollowing"
+              @click="destroy"
+          >フォロー解除</button>
+          <button
+              v-else
+              class="c-btn c-btn__follow"
+              :class="{'c-btn__disabled': isAutoFollowing}"
+              :disabled="isAutoFollowing"
+              @click="follow"
+          >フォローする</button>
+        </div>
+      </div>
 
-          <!-- FF数 -->
-          <div class="item-5 p-accounts__ff--area">
+      <!-- Twitterプロフィール -->
+      <div
+          class="item-5 p-accounts__profile--description"
+          v-if="isExist_profile_description"
+      >
+        <p>{{ account.description }}</p>
+      </div>
 
-            <div class="item-6 p-accounts__ff--item">
-              <div class="item-7 p-accounts__ff--count">
-                <p>
-                  <a
+      <!--  -->
+      <div class="item-4 p-accounts__follow--area">
+        <!-- FF数 -->
+        <div class="item-5 p-accounts__ff--area">
+
+          <div class="item-6 p-accounts__ff--item">
+            <div class="item-7 p-accounts__ff--title"><p>フォロー</p></div>
+            <div class="item-7 p-accounts__ff--count">
+              <p>
+                <a
                     :href="twitter_following_url"
                     target="_blank"
                     rel="noopener noreferrer"
-                  >
-                    {{ account.friends_count }}
-                  </a>
-                </p>
-              </div>
-              <div class="item-7 p-accounts__ff--title"><p>フォロー中</p></div>
+                >
+                  {{ account.friends_count }}
+                </a>
+              </p>
             </div>
-            <div class="item-6 p-accounts__ff--item">
-              <div class="item-7 p-accounts__ff--count">
-                <p>
-                  <a
-                      :href="twitter_followers_url"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                  >
-                    {{ account.followers_count }}
-                  </a>
-                </p>
-              </div>
-              <div class="item-7 p-accounts__ff--title"><p>フォロワー</p></div>
+          </div>
+
+          <div class="item-6 p-accounts__ff--item">
+            <div class="item-7 p-accounts__ff--title"><p>フォロワー</p></div>
+            <div class="item-7 p-accounts__ff--count">
+              <p>
+                <a
+                    :href="twitter_followers_url"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                  {{ account.followers_count }}
+                </a>
+              </p>
             </div>
           </div>
         </div>
@@ -314,23 +313,23 @@ export default {
   }
 }
 
-.item-2 {
-  /*background: #c5ccd9;*/
-}
-.item-3 {
-  /*background: #bfabcb;*/
-}
-.item-4 {
-  /*background: #c4ceff;*/
-}
-.item-5 {
-  /*background: #d2eed5;*/
-}
-.item-6 {
-  /*background: #ffdaf7;*/
-}
-.item-7 {
-  /*background: #b5fff0;*/
-}
+/*.item-2 {*/
+/*  background: #c5ccd9;*/
+/*}*/
+/*.item-3 {*/
+/*  background: #bfabcb;*/
+/*}*/
+/*.item-4 {*/
+/*  background: #c4ceff;*/
+/*}*/
+/*.item-5 {*/
+/*  background: #d2eed5;*/
+/*}*/
+/*.item-6 {*/
+/*  background: #ffdaf7;*/
+/*}*/
+/*.item-7 {*/
+/*  background: #b5fff0;*/
+/*}*/
 
 </style>
