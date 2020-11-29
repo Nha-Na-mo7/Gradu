b3468c4595b8b9e3758629daaab084a1c1725c71
Nhala <zirconiump42@gmail.com>
2020-11-29 20:50:40 +0900

    sass twitter連携していないときにアカウント一覧を見ようとした時の処理ページ

--- resources/js/pages/AccountLists/NeedLinkage.vue
+++ resources/js/pages/AccountLists/NeedLinkage.vue
@@ -6,15 +6,23 @@
     <h2 class="">{{ title }}</h2>
     <p>{{ description }}</p>
 
-    <div class="renkei">
-      <p>今すぐTwitterアカウントと連携</p>
+    <div class="u-mt-xl">
+      <button class="c-btn c-btn__twitter">
+        <a
+            title="Start for Twitter!"
+            @click.stop
+            :href="`/twitter/auth/begin`"
+        >
+          今すぐTwitterと連携する
+        </a>
+      </button>
     </div>
   </div>
 </template>
 
 <script>
 const NOTHING_ACCOUNT_TITLE = '「アカウント一覧」のご利用にはTwitter連携が必要です。';
-const NOTHING_ACCOUNT_DESCRIPTION = 'Twitterと連携していただくことによって、仮想通貨アカウントのフォロー・自動フォロー機能が利用可能になります。';
+const NOTHING_ACCOUNT_DESCRIPTION = 'Twitterと連携することで、仮想通貨アカウントのフォロー・自動フォロー機能が利用可能になります。';
 
 export default {
   data() {
