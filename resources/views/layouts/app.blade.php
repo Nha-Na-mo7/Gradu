62195095c5ad79c5ff2ac2661f7d3dbfe5061149
Nhala <zirconiump42@gmail.com>
2020-12-03 03:18:31 +0900

    電話番号リンクを作らない

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="format-detection" content="telephone=no">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -14,6 +15,7 @@
     <meta name="description" itemprop="description" content="@yield('description')">
     <!-- キーワード -->
     <meta name="keyword" itemprop="keyword" content="@yield('keyword')">
+
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
