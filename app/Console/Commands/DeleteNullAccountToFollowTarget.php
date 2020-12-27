c5f44da6277b7d1014ba1029aa96861a5ebd294b
Nhala <zirconiump42@gmail.com>
2020-12-27 09:30:32 +0900

    アカウントIDがNullのレコードを削除するコマンドを記述

--- app/Console/Commands/DeleteNullAccountToFollowTarget.php
+++ app/Console/Commands/DeleteNullAccountToFollowTarget.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use App\Http\Controllers\TwitterAccountListController;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class DeleteNullAccountToFollowTarget extends Command
 {
@@ -37,6 +39,17 @@ class DeleteNullAccountToFollowTarget extends Command
      */
     public function handle()
     {
-        //
+      Log::debug('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
+      Log::debug('Console/Commands DeleteNullAccountToFollowTarget アカウントIDがNULLのレコードを削除');
+      Log::debug('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
+      // TrendTweetControllerのインスタンスを取得
+      $trendTweetController = new TwitterAccountListController();
+  
+      // follow_targetsテーブル内、account_idがNULLのレコードを全て削除する
+      $trendTweetController->delete_nullaccount_follow();
+  
+      Log::debug('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
+      Log::debug('定刻のアカウントIDがNULLのレコード削除を終了します。');
+      Log::debug('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
     }
 }
